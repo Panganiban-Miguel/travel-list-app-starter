7294ca78d70c18ed0099c6c9b4b8ad96b459338e
Panganiban Miguel <24022674@myrp.edu.sg>
2025-12-02 13:02:56 +0800

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import logo from "./components/logo.js";
+import Logo from "./components/logo.js";
 
 // Initial packing items
 const initialItems = [
@@ -7,20 +7,11 @@ const initialItems = [
   { id: 2, description: "Pants", quantity: 2, packed: false },
 ];
 
-// L13 Activity 8a
-const[items, setItems] = useState([])
-
-// L13 Activity 8b
-function handleAddItems(item){
-  setItems([...items, item])
-
-}
-
 // function Logo() {
 //   return <h1>My Travel List</h1>;
 // }
 
-function Form() {
+function Form({description, onNewDescription, quantity, onNewQuantity, onAddItems}) {
   function handleSubmit(e){
     e.preventDefault();
 
@@ -36,61 +27,124 @@ function Form() {
 
     onAddItems(newItem);
 
-    setQuantity(1);
-    setDescription("");  
+    onNewQuantity(1);
+    onNewDescription("");  
   }
-  
-  const [quantity, setQuantity] = useState(1)
-  const [description, setDescription] = useState('')
-  const [packed, setPacked] = useState(false)
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
+
       <h3>What do you need to pack?</h3>
-      <select onChange={(e)=>setQuantity(e.target.value)}>
+
+      <select onChange={(e)=>onNewQuantity(Number(e.target.value))}>
+
         <option value={1}>1</option>
         <option value={2}>2</option>
         <option value={3}>3</option>
+
       </select>
-      <input value={description} onChange={(e)=>setDescription(e.target.value)} />
+
+      <input type='text'
+        placeholder="Item..."
+        value={description}
+        onChange={(e)=>onNewDescription(e.target.value)}
+      />
+
       <button type="submit">ADD</button>
+
     </form>
   );
 }
 
-function Item(quantity, description){
+function Item({item, onDeleteItem, onToggleItem}) {
   return (
-    packed === true ? <p>{description}({quantity})</p> : <p style='strike-through'>{description}({quantity})</p>
+  //   packed === true ? <li>{description}({quantity})</li> : <li style='strike-through'>{description}({quantity})</li>
+  <li>
+    <input
+    type="checkbox"
+    checked={item.packed}
+    onChange={() => onToggleItem(item.id)}/>
+    <span>
+      {item.description} ({item.quantity})
+    </span>
+    <button onClick={() => onDeleteItem(item.id)}>❌</button>
+  </li>
   )
 }
 
-function PackingList() {
+function PackingList({items, onDeleteItem, onToggleItem}) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => {
-          <Item quantity={item.quantity} description={item.description} />
-        })}
+        {items.map((item) => (
+          <Item
+          key={item.id}
+          item={item}
+          onDeleteItem={onDeleteItem}
+          onToggleItem={onToggleItem}/>
+        ))}
       </ul>
     </div>
   );
 }
 
-function Stats() {
+function Stats(items) {
+  if (!items.length) {
+    return (
+      <footer className="stats">
+        <p>Your packing list is empty. Start adding some items!</p>
+      </footer>
+    )
+  }
+
   return (
     <footer className="stats">
-      <em>You have {items.length} items in the list. You already packed Y ({TempReplaceWithTotalPacked / items.length * 100}%).</em>
+      <>
+      {items.filter((item) => item.packed).length === 100 ? "Nice, you're done with everything!" : 
+      `You have ${items.length} items in the list, and have already packed ${(items.filter((item)=>item.packed).length) / items.length * 100}%.`}
+    </>
     </footer>
   );
 }
 
 function App() {
+  // L13 Activity 8a
+  const[items, setItems] = useState(initialItems)
+  const [quantity, setQuantity] = useState(1)
+  const [description, setDescription] = useState('')
+
+  // L13 Activity 8b
+  function handleAddItems(item){
+    setItems([...items, item])
+  }
+
+  function handleDeleteItem(id) {
+    setItems((cur) => cur.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((cur) =>
+      cur.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
-      <Stats />
+      <Form
+      description={description}
+      onNewDescription={setDescription}
+      quantity={quantity}
+      onNewQuantity={setQuantity}
+      onAddItems={handleAddItems}
+      />
+      <PackingList
+      items={items}
+      onDeleteItem={handleDeleteItem}
+      onToggleItem={handleToggleItem}/>
+      <Stats items={items}/>
     </div>
   );
 }
